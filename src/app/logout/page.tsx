7c99851c43915bce79655eb1f76f02ce00707021
Asad Iqbal <160511167+Asad2014@users.noreply.github.com>
2025-02-20 14:24:09 +0500

    first commit

--- src/app/logout/page.tsx
+++ src/app/logout/page.tsx
@@ -1,3 +1,35 @@
+// "use client"; 
+// import { useEffect } from "react";
+// import { signOut } from "firebase/auth";
+// import { auth } from "@/firebaseConfig"; 
+// import { useRouter } from "next/navigation";
+
+// export default function LogoutPage() {
+//   const router = useRouter();
+
+//   useEffect(() => {
+//     const logoutUser = async () => {
+//       try {
+//         await signOut(auth); // 🔥 Firebase Logout
+//         router.push("/signin"); // 🔄 Redirect to Sign In page
+//       } catch (error: any) {
+//         console.error("Logout Error:", error.message);
+//       }
+//     };
+
+//     logoutUser();
+//   }, [router]);
+
+//   return (
+//     <div className="flex items-center justify-center min-h-screen bg-gray-100">
+//       <p className="text-lg font-semibold text-gray-700">Logging out...</p>
+//     </div>
+//   );
+// }
+
+
+
+
 "use client"; 
 import { useEffect } from "react";
 import { signOut } from "firebase/auth";
@@ -8,16 +40,7 @@ export default function LogoutPage() {
   const router = useRouter();
 
   useEffect(() => {
-    const logoutUser = async () => {
-      try {
-        await signOut(auth); // 🔥 Firebase Logout
-        router.push("/signin"); // 🔄 Redirect to Sign In page
-      } catch (error: any) {
-        console.error("Logout Error:", error.message);
-      }
-    };
-
-    logoutUser();
+    signOut(auth).then(() => router.push("/signin"));
   }, [router]);
 
   return (
