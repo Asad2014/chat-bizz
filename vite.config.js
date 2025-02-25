0437792ed148387946e96e91368abe26eaac011f
Asad Iqbal <160511167+Asad2014@users.noreply.github.com>
2025-02-25 13:40:22 +0500

    updated

--- vite.config.js
+++ vite.config.js
@@ -2,8 +2,13 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),    tailwindcss()  ],
+  plugins: [react(), tailwindcss()],
+  server: {
+    host: '0.0.0.0', // Optional for dev mode
+    port: process.env.PORT || 5173, // Fallback for local dev
+  },
+  build: {
+    outDir: 'dist', // Matches Railway’s default expectation
+  },
 })
